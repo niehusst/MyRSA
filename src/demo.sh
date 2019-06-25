43a1e09b5aa87c20710fff4df21426e26657e3a0
niehusst <liamniehusstaab@gmail.com>
2019-06-24 22:17:17 -0700

    added encryption code to client server. Now just need to fix networking bugs

--- src/demo.sh
+++ src/demo.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-## A simple test of the echo server code in this dir
+## A simple test of the echo server code (using RSA encryption) in this dir
 ## Author: Liam Niehus-Staab
 
 # make sure everything is made and up to date
-make all
+make
 
 # start the server running (run in bg-thread so we can run a client in the same window)
 ./server &
+#//TODO network code in demo.sh is failing because client is being launched b4 server sets up bcus of rsa stuff
+sleep 1
 
-# start the client program (running localhost as the other network bc 
+# start the client program (running localhost as the other network bc
 #   I only have 1 computer)
 ./client localhost <<< 'Hello World!'
-
 # ^^ tell client what message to send to server
 
-
 # the echo server should respond by outputting Hello World! back to us
+make clean
--- tests/run_test_suite.sh
+++ tests/run_test_suite.sh
@@ -3,6 +3,7 @@
 # make test suites
 make
 
+echo "Expected runtime: 1.5 minutes"
 # run key tests
 time ./key_tests
 # run padding tests
